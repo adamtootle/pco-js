a3088c6c71c56a7e37cc345f19889d4b06537aaa
Adam Tootle <adamtootle@gmail.com>
2017-06-06 17:37:27 -0400

    Removing some unused/commented code.

--- src/services/plans.js
+++ src/services/plans.js
@@ -1,36 +1,5 @@
 import Promise from 'bluebird';
-import forEach from 'lodash/forEach';
-import flatten from 'lodash/flatten';
 import http from '../http';
-import Schedules from './schedules';
-
-function getPlanItems(schedule, plan) {
-  const newPlan = plan;
-  return new Promise((resolve, reject) => {
-    http.get(`/service_types/${schedule.relationships.service_type.data.id}/plans/${plan.id}/items?include=media,song`)
-      .then((res) => {
-        newPlan.items = res.data;
-        resolve(newPlan);
-      })
-      .catch((err) => {
-        reject(err);
-      });
-  });
-}
-
-function getPlanAttachments(schedule, plan) {
-  const newPlan = plan;
-  return new Promise((resolve, reject) => {
-    http.get(`/service_types/${schedule.relationships.service_type.data.id}/plans/${plan.id}/all_attachments`)
-      .then((res) => {
-        newPlan.attachments = res.data;
-        resolve(newPlan);
-      })
-      .catch((err) => {
-        reject(err);
-      });
-  });
-}
 
 class Plans {
   get = (planId) => (
@@ -56,22 +25,6 @@ class Plans {
     return new Promise((resolve) => {
       http.get(plan.links.all_attachments)
         .then((attachments) => {
-          // const planAttachments = flatten(attachmentsRes.data.map((singleAttachmentsResponse) => {
-          //   return singleAttachmentsResponse.data.map((attachment) => {
-          //     return {
-          //       ...attachment,
-          //       relationships: {
-          //         ...attachment.relationships,
-          //         song: {
-          //           ...attachment.relationships.song,
-          //           data: {
-          //             id: singleAttachmentsResponse.meta.parent.id,
-          //           },
-          //         },
-          //       },
-          //     };
-          //   });
-          // }));
           resolve(attachments.data);
         });
     });
