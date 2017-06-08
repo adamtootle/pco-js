b318e48745c561076846c05e3b1ef24a98f87aac
Adam Tootle <adamtootle@gmail.com>
2017-06-07 23:05:51 -0400

    Updating the name in package.json and changing the version to 0.1.0 since this supports so little right now.

--- dist/services/plans.js
+++ dist/services/plans.js
@@ -8,49 +8,13 @@ var _bluebird = require('bluebird');
 
 var _bluebird2 = _interopRequireDefault(_bluebird);
 
-var _forEach = require('lodash/forEach');
-
-var _forEach2 = _interopRequireDefault(_forEach);
-
-var _flatten = require('lodash/flatten');
-
-var _flatten2 = _interopRequireDefault(_flatten);
-
 var _http = require('../http');
 
 var _http2 = _interopRequireDefault(_http);
 
-var _schedules = require('./schedules');
-
-var _schedules2 = _interopRequireDefault(_schedules);
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function getPlanItems(schedule, plan) {
-  var newPlan = plan;
-  return new _bluebird2.default(function (resolve, reject) {
-    _http2.default.get('/service_types/' + schedule.relationships.service_type.data.id + '/plans/' + plan.id + '/items?include=media,song').then(function (res) {
-      newPlan.items = res.data;
-      resolve(newPlan);
-    }).catch(function (err) {
-      reject(err);
-    });
-  });
-}
-
-function getPlanAttachments(schedule, plan) {
-  var newPlan = plan;
-  return new _bluebird2.default(function (resolve, reject) {
-    _http2.default.get('/service_types/' + schedule.relationships.service_type.data.id + '/plans/' + plan.id + '/all_attachments').then(function (res) {
-      newPlan.attachments = res.data;
-      resolve(newPlan);
-    }).catch(function (err) {
-      reject(err);
-    });
-  });
-}
 
 var Plans = function Plans() {
   _classCallCheck(this, Plans);
